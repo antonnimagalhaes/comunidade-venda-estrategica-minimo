46010fb6916c3c040af7b4a28e60b6f531d6b000
Comunidade Venda Estratégica <antonni.magalhaes@gmail.com>
2025-06-02 21:03:41 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
     </>
   )
 }
